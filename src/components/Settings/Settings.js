16fa671b4bd2c250de5f373ad3d9273db08a216c
mdm106 <mads.mcmahon@googlemail.com>
2020-06-20 12:09:24 +0100

    Settings form component created

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,111 @@
+import React, { component } from "react";
+
+class Settings extends Component {
+    constructor(props) {
+        super(props);
+
+        // initial state with default values has been mapped to props in index file
+        // local state set to these values
+        this.state = {
+            team1Name: props.team1Name,
+            team2Name: props.team2Name,
+            teamSize: props.teamSize,
+            abilityPick: props.abilityPick,            
+        };
+
+        this.handleTeam1Name = this.handleTeam1Name.bind(this);
+        this.handleTeam2Name = this.handleTeam2Name.bind(this);
+        this.handleTeamSize = this.handleTeamSize.bind(this);
+        this.handleAbilityPick = this.handleAbilityPick.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleTeam1Name(e) {
+        this.setState({ team1Name: e.currentTarget.value });
+    }
+
+    handleTeam2Name(e) {
+        this.setState({ team2Name: e.currentTarget.value });
+    }
+
+    handleTeamSize(e) {
+        this.setState({ teamSize: e.currentTarget.value });
+    }
+
+    handleAbilityPick(e) {
+        const target = e.target;
+        const value = target.name === 'abilityPick' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.handleSettings({ ...this.state });
+    }
+
+    render() {
+        let {
+            team1Name,
+            team2Name,
+            teamSize,
+            abilityPick, 
+        } = this.state;
+
+        return (
+            <form>
+                <div className="form-group">
+                    <label for="team1Name">Team 1 Name:</label>
+                    <input value={this.state.team1Name}
+                           type="text"
+                           className="form-control"
+                           id="team1Name"
+                           placeholder="Team 1 Name"
+                           onChange={this.handleTeam1Name}></input>
+                </div>
+                <div className="form-group">
+                    <label for="team2Name">Team 2 Name:</label>
+                    <input value={this.state.team2Name}
+                           type="text"
+                           className="form-control"
+                           id="team2Name"
+                           placeholder="Team 2 Name"
+                           onChange={this.handleTeam2Name} />
+                </div>
+                <div className="form-group">
+                    <label for="teamSize">Players per team:</label>
+                    <select value={this.state.teamSize}
+                            className="form-control"
+                            id="exampleFormControlSelect1"
+                            onChange={this.handleTeamSize}>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option selected value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                    </select>
+                </div>
+                <div class="form-check">
+                    <label>
+                        Select teams by ability:
+                        <input
+                            name="abilityPick"
+                            type="checkbox"
+                            checked={this.state.abilityPick}
+                            onChange={this.handleAbilityPick} />
+                    </label>
+                </div>
+            </form>
+        )
+    }
+
+
+}

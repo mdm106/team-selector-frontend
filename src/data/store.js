44469386ae2780611c69b61aa61ff00e7d85862d
mdm106 <mads.mcmahon@googlemail.com>
2020-06-20 10:33:45 +0100

    Store reformatted and comments added

--- src/data/store.js
+++ src/data/store.js
@@ -4,12 +4,14 @@ import thunk from "redux-thunk";
 import initial from "./initial";
 import reducer from "./reducer";
 
-const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-        const store = createStore(
-            reducer,
-            initial,
-            composeEnhancers(applyMiddleware(thunk))
+// set up redux dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// set up store
+const store = createStore(
+    reducer,
+    initial,
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;

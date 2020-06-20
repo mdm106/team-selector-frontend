7daaac6365527cc1bd781a558c0eb3bf7ad00909
mdm106 <mads.mcmahon@googlemail.com>
2020-06-20 12:40:01 +0100

    saveSettings reducer method added to reducer

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,5 +1,20 @@
+const saveSettingsReducer = (state, 
+    {team1Name,
+    team2Name,
+    teamSize,
+    abilityPick }) => {
+        return {
+            ...state,
+            team1Name,
+            team2Name,
+            teamSize,
+            abilityPick,
+        }
+    }
+
 const reducer = (state, action) => {
     switch (action.type) {
+        case "SAVE_SETTINGS": return saveSettingsReducer(state, action);
         default: return state;
     }
 }

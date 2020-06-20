474b176b4b1f2a9cd07d9122c1843a510b70e89a
mdm106 <mads.mcmahon@googlemail.com>
2020-06-20 16:01:25 +0100

    React router and history imported and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
 
+//ReactRouter component imports
+import {
+  Router,
+  Route,
+} from "react-router-dom";
+// History file imported to use ReactRouter's history functionality
+import history from "./history";
+
+//Component imports
 import Header from "./components/Header";
 import Settings from "./components/Settings";
 
 const App = () => (
   <>
-    <Header />
-    <Settings />
+    <Router history={ history }>
+      <Header />
+      <Settings />
+    </Router>
   </>
 );
 

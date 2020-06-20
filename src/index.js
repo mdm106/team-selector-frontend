cdbe4666646c163b1f12daa3fc7428aa79ef0159
mdm106 <mads.mcmahon@googlemail.com>
2020-06-20 10:20:03 +0100

    store, provider and redux imports imported to index.js and App wrapped in provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import store from "../src/data/store";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={ store }>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>, document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change

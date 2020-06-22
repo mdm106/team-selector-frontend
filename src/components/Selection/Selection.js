653b857f759aeb5c18df33de257ab5b14b071da9
mdm106 <mads.mcmahon@googlemail.com>
2020-06-22 16:16:25 +0100

    Selection component updated to display the two teams details and index container updated to send correct state as props

--- src/components/Selection/Selection.js
+++ src/components/Selection/Selection.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-const Selection = ({ team1Name, team2Name, team1, team2 }) => {
+const Selection = ({ team1Name, team2Name, teams, abilityPick }) => {
 
     return (
         <>
         <h2>Your team selection is:</h2>
+            {teams.map((value, index) => (
+                <div key={ index } className="container">
+                <li>{index === 0 ? team1Name : team2Name}</li>
+                {value.map((player, index) => (
+                    <li key={ index }>
+                        <p>Name: {player.name}</p>
+                        {abilityPick ?
+                        <p>Ability: {player.ability}/100</p>
+                        : null }
+                    </li>
+                ))}
+                </div>
+            ))}
         </>
     );
 };
+
+export default Selection;
--- src/components/Selection/index.js
+++ src/components/Selection/index.js
@@ -2,19 +2,13 @@ import { connect } from "react-redux";
 
 import Selection from "./Selection";
 
-const mapStateToProps = ({ team1Name, team2Name, team1, team2 }) => {
+const mapStateToProps = ({ team1Name, team2Name, teams, abilityPick }) => {
     return {
         team1Name,
         team2Name,
-        team1,
-        team2,
+        teams,
+        abilityPick,
     };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Selection);
+export default connect(mapStateToProps)(Selection);

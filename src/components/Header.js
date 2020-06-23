7cf544b07e5bf9aa476c63f03d2c0d209ed6a4e0
mdm106 <mads.mcmahon@googlemail.com>
2020-06-23 22:07:31 +0100

    Putting header in container div

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Header = () => (
-    <header className="jumbotron">
-        <h1>Team Selector</h1>
-    </header>
+    <div class="container">
+        <header className="jumbotron">
+            <h1>Team Selector</h1>
+        </header>
+    </div>
 );
 
 export default Header;

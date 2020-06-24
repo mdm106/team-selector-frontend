0a50f048b9e5b1010866475f54b2e47865b067df
mdm106 <mads.mcmahon@googlemail.com>
2020-06-24 12:55:42 +0100

    Made list items into flex and space-between

--- src/components/Selection/Selection.js
+++ src/components/Selection/Selection.js
@@ -13,9 +13,9 @@ const Selection = ({ team1Name, team2Name, teams, abilityPick }) => {
                 </ul>
                 {team.map((player, index) => (
                     <li className="list-group-item" key={ index }>
-                        <p>{player.name}</p>
+                        <p className="list-item-name">{player.name}</p>
                         {abilityPick ?
-                        <p>Ability: {player.ability}/100</p>
+                        <p className="list-item-ability">Ability: {player.ability}/100</p>
                         : null }
                     </li>
                 ))}

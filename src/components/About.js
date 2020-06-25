233f721a96fe9b89f83350fd38c262966533fc09
mdm106 <mads.mcmahon@googlemail.com>
2020-06-25 17:46:39 +0100

    Text for about page added

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const About = () => (
     <div className="container">
-        <h3 className="instructions-heading">About</h3>
-        <ul><p>Team Selector helps you quickly and fairly pick your teams.</p>
-            <li>Team name generator for </li>
-        
-        </ul>
+        <h5>Team Selector helps you quickly and fairly pick your teams. Less time organising, more time winning.</h5>
+        <br/>
+            <ul>
+            <h5>Features include:</h5>
+                <li>Team name generator to randomly assign your teams with fun names</li>
+                <li>Selection for team sizes ranging from 5-a-side up to 11-a-side</li>
+                <li>Random selection of teams</li>
+                <li>Option to provide player ability scores for balanced team selection</li>
+                <li>Copy feature so that you can easily paste the team details into email, text messages etc</li>      
+            </ul>
         
     </div>
 );

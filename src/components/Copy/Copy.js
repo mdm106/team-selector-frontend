e90f172a1fccf8cf3c7a37fe01c28514c3e384bd
mdm106 <mads.mcmahon@googlemail.com>
2020-06-25 15:10:58 +0100

    Removing input from imports and refactoring concat

--- src/components/Copy/Copy.js
+++ src/components/Copy/Copy.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import Input from "../FormControls/Input";
 
 class Copy extends Component {
     constructor(props) {
@@ -9,7 +8,7 @@ class Copy extends Component {
         let makeText = (team1Name, team2Name, teams) => {
             let team1 = teams[0].reduce((string, player) => string + player.name + "\n", "\n");
             let team2 = teams[1].reduce((string, player) => string + player.name + "\n", "\n");
-            return "Team: " + team1Name + team1 + "\n" + "Team: " + team2Name + team2;
+            return "Team: " + team1Name + team1 + "\nTeam: " + team2Name + team2;
         }
 
         this.state = {

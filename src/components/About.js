7699be0fa94294642c142b3fffdbbdb80fbebbe0
mdm106 <mads.mcmahon@googlemail.com>
2020-06-26 23:28:25 +0100

    Additional features added as bullet points to about page

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,10 @@ const About = () => (
                 <li>Selection for team sizes ranging from 5-a-side up to 11-a-side</li>
                 <li>Random selection of teams</li>
                 <li>Option to provide player ability scores for balanced team selection</li>
-                <li>Copy feature so that you can easily copy the team details to paste into email, text messages etc</li>      
+                <li>Copy feature so that you can easily copy the team details to paste into email, text messages etc</li> 
+                <li>Save game details, including the game date and team names</li>  
+                <li>Update game details with the score, when the game is complete</li>  
+                <li>View the details from previous games</li>  
             </ul>
             <div className="central-button">
                 <Link to={`/team-details`}>

85e03c97d2ffd7f51f6c74cdf7c44f093b4f74f3
mdm106 <mads.mcmahon@googlemail.com>
2020-06-26 23:58:35 +0100

    Message and styling added to loading page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Loading extends Component {
   render() {
     const { children, loaded } = this.props;
     return loaded ? children : (
+      <>
       <div className="progress">
         <div
           className="progress-bar progress-bar-striped progress-bar-animated"
         />
       </div>
+      <div className="loading-message">
+        <p>Been here a while? Something might have gone wrong. Click the button to go to home</p>
+        <Link to={`/`}>
+          <p className="btn btn-danger">Home</p>
+        </Link>
+      </div>
+      </>
     );
   }
 }

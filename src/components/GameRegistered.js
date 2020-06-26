39bf5a5293c492217b53361da9ab8a7fd1adc986
mdm106 <mads.mcmahon@googlemail.com>
2020-06-26 23:18:23 +0100

    Button to go to game history added to game saved page

--- src/components/GameRegistered.js
+++ src/components/GameRegistered.js
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 const GameRegistered = () => (
     <div className="container">
         <h1>Your game has been saved!</h1>
-        <h2 className="titleFont contrastHeading">View game history (link to history when available)</h2>
-        <Link to={`/`}>
-            <div className="central-button">
-                <p className="btn btn-primary">Take me home</p>
-            </div>
-        </Link>
+        <div className="adjacent-buttons">
+            <Link to={`/`}>
+                    <p className="btn btn-primary">Take me home</p>
+            </Link>
+            <Link to={`/game-history`}>
+                <p className="btn btn-danger">View previous game details</p>
+            </Link>
+        </div>
     </div>
 )
 

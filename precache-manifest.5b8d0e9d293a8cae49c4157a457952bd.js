a58ca33ca9319589acb84ff614ba138aa1188df6
mdm106 <mads.mcmahon@googlemail.com>
2020-06-27 13:25:30 +0100

    Updates

---
+++ precache-manifest.5b8d0e9d293a8cae49c4157a457952bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6115f8566954b3ba370e43914eb568",
+    "url": "/team-selector-frontend/index.html"
+  },
+  {
+    "revision": "bd9ee22b152dddad0d06",
+    "url": "/team-selector-frontend/static/css/main.1a71a358.chunk.css"
+  },
+  {
+    "revision": "5558f656597af53f4098",
+    "url": "/team-selector-frontend/static/js/2.2e8f5377.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/team-selector-frontend/static/js/2.2e8f5377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9ee22b152dddad0d06",
+    "url": "/team-selector-frontend/static/js/main.3da0742d.chunk.js"
+  },
+  {
+    "revision": "db8f96799c1a8a679f08",
+    "url": "/team-selector-frontend/static/js/runtime-main.e46b5cbc.js"
+  },
+  {
+    "revision": "31a3620c63d99c244b0e5b9250c08880",
+    "url": "/team-selector-frontend/static/media/evening.31a3620c.jpg"
+  }
+]);
